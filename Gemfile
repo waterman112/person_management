7696653c2324c691e86c85f75214bbac58b40d18
Lijun Liu <lijun.liu@classiclaw.com>
2016-11-28 11:40:00 +0800

    初始化一个git仓库

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '4.2.1'
+
+gem 'mysql','0.3.17'
+
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'bootstrap-sass', '~> 3.3.5'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonManagement::Application.config.secret_token = '53afa403aaa9c8a8734f31cec30a84105699ca8f5d795884235c3b86c8f32cf3c01349180c556aed04625f47d75d52a07163e8b9640bb3a572d3ea0888526227'
